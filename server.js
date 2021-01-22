d46b2493112381e1b0ad63a5b00227295354d494
Ricky <ricardo.kleine.samson@gmail.com>
2021-01-22 16:03:24 -0300

    final commit.

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.get('/iceServers', async (req, res) => {
 
 })
 
-app.get(['/', '/:room'], (req, res) =>
+app.get(['/', '/:room', '*'], (req, res) =>
   res.sendFile(path.join(__dirname, 'www/index.html'))
 )
 
